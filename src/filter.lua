a695c135a21a098394b405738c958dc3e5dec3d8
Virginia Senioria <91khr@users.noreply.github.com>
2022-10-15 18:27:33 +0800

    fix out: use filter to hide the out patch

---
+++ src/filter.lua
@@ -0,0 +1,12 @@
+local base = ...
+local files = list_dir(base)
+local articles = {}
+local subdirs = {}
+for _, file in pairs(files) do
+    if file.type == "file" and file.path:match(".md$") then
+        articles[#articles + 1] = file.path
+    elseif file.type == "dir" and file.path ~= "src/out" then
+        subdirs[#subdirs + 1] = file.path
+    end
+end
+return { articles = articles, subdirs = subdirs }
